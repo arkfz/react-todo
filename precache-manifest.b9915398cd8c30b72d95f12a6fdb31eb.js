dfa48fa3ef2bb3d9d50fd9bd52e89a80bb2e9ad3
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-04-15 20:48:10 +0200

    Updates

---
+++ precache-manifest.b9915398cd8c30b72d95f12a6fdb31eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "e69ba9380ca9497f1615",
+    "url": "/react-todo/static/js/main.8b177f85.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-todo/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e69ba9380ca9497f1615",
+    "url": "/react-todo/static/css/main.060d3aea.chunk.css"
+  },
+  {
+    "revision": "107c906506559541424ef1a1b701b95d",
+    "url": "/react-todo/index.html"
+  }
+];
